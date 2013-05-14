0a270d4a0f5b540f726c68051dd7de4b81912bf7
Robert Pendell <shinji@elite-systems.org>
2013-05-14 06:47:45 -0400

    PREP: Added new config vars for opme trap to be implemented...

--- src/main/java/org/shinji257/aop/aOP.java
+++ src/main/java/org/shinji257/aop/aOP.java
@@ -36,9 +36,24 @@ public class aOP extends JavaPlugin {
         if (!getConfig().contains("opdrop")) getConfig().set("opdrop", true);
         if (!getConfig().contains("notify")) getConfig().set("notify", true);
         if (!getConfig().contains("silent")) getConfig().set("silent", false);
-        if (!getConfig().contains("opmetrap")) getConfig().set("opmetrap", false);
+        if (!getConfig().contains("opmetrap.enabled")) getConfig().set("opmetrap.enabled", false);
+        // New stuff for opme trap above... opme is still being worked on ofc.
+        // Vault was added for possible functionality for opme trap...
+        if (!getConfig().contains("opmetrap.teleport.enabled")) getConfig().set("opmetrap.teleport.enabled", false);
+        if (!getConfig().contains("opmetrap.teleport.posx")) getConfig().set("opmetrap.teleport.posx", 0);
+        if (!getConfig().contains("opmetrap.teleport.posy")) getConfig().set("opmetrap.teleport.posy", -200);
+        if (!getConfig().contains("opmetrap.teleport.posz")) getConfig().set("opmetrap.teleport.posz", 0);
+        if (!getConfig().contains("opmetrap.godmode")) getConfig().set("opmetrap.godmode", true);
+        if (!getConfig().contains("opmetrap.blockchat")) getConfig().set("opmetrap.blockchat", 2);
+        if (!getConfig().contains("opmetrap.eco.enabled")) getConfig().set("opmetrap.eco.enabled", false);
+        if (!getConfig().contains("opmetrap.eco.cost")) getConfig().set("opmetrap.eco.cost", 50);
+        if (!getConfig().contains("opmetrap.explosion")) getConfig().set("opmetrap.explosion", false);
+        if (!getConfig().contains("opmetrap.lightning")) getConfig().set("opmetrap.lightning", false);
+        if (!getConfig().contains("opmetrap.kill")) getConfig().set("opmetrap.kill", false);
+        if (!getConfig().contains("opmetrap.kick")) getConfig().set("opmetrap.kick", false);
 
-        // Save default values to config.yml in datadirectory
+
+        // Save default values to config.yml
         saveConfig();
     }
 }
